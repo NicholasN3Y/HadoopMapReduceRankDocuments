4153671ac11c2f3693d0f130975581d52521bf77
mininet <nicholas.n3y@gmail.com>
2017-02-12 23:21:39 -0800

    refined run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+source=$1
+dest=$2
+sw=$3
+query=$4
+K=$5
+
+if [[ $# -lt 4 ]] ; then
+    echo "Usage run.sh <source-path> <dest-path> <stop-words-file-path> <query-file-path> [K]"
+    exit 1
+fi
+
 cd src/assgn1/cs4225
 echo -e  "\n########## compiling java code ##########\n"
 javac -cp `hadoop classpath` RankTopK.java -d ../../../bin
@@ -8,14 +19,14 @@ cd ../../../bin
 echo -e  "\n########## creating jar file ##########\n"
 jar -cvf RankTopK.jar .
 echo -e  "\n########## clean previous hadoop Job output directory ##########\n"
-hadoop fs -rm -R a0112224/assignment_0/output
+hadoop fs -rm -R $dest
 hadoop fs -rm -R a0112224/assignment_0/intermediate_result
 
 echo -e  "\n########## start hadoop MapReduce Job ##########\n"
-hadoop jar RankTopK.jar assgn1.cs4225.RankTopK a0112224/assignment_0/input a0112224/assignment_0/output sw4.txt query.txt
+hadoop jar RankTopK.jar assgn1.cs4225.RankTopK $source $dest $sw $query $K
 echo -e  "\n########## get output results from hadoop fs ##########\n"
 rm ../final_results.txt
-hadoop fs -get a0112224/assignment_0/output/part-r-00000 ../final_results.txt
+hadoop fs -get $dest/part-r-00000 ../final_results.txt
 rm ../1.txt
 hadoop fs -get a0112224/assignment_0/intermediate_result/1/part-r-00000 ../1.txt
 rm ../2.txt
